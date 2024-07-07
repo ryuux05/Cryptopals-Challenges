a88467d52562da82d4790e82dc1b54bfb50b70a1
ryuux05 <111038563+ryuux05@users.noreply.github.com>
2024-07-07 10:28:12 +0900

    Initial commit

---
+++ CMakeFiles/set1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/set1.dir/set1.c.o"
+  "CMakeFiles/set1.dir/set1.c.o.d"
+  "set1"
+  "set1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/set1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is challenges for cryptopals.com
